8853d3b4e2c97684925ebba29d1b17ebfad228fe
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-09-29 20:11:13 +0200

    fixed

---
+++ src/main/java/com/example/javata/exceptions/AgeValidationException.java
@@ -0,0 +1,7 @@
+package com.example.javata.exceptions;
+
+public class AgeValidationException extends RuntimeException {
+    public AgeValidationException(String message) {
+        super(message);
+    }
+}
